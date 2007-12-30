db7c45193b51543e1560fc7bc7db8a7688438260
jvaughan <jvaughan@31e0cd6d-f31f-0410-8c93-df718c08c370>
2007-12-30 15:24:05 +0000

    working for determining offset from a start note, but returns notes like E#
    git-svn-id: svn+ssh://ln.turnip.org.uk/var/svn/jvaughan/trunk/dev/harplayout@121 31e0cd6d-f31f-0410-8c93-df718c08c370

--- modules/Harmonica/CircleOfFifths.pm
+++ modules/Harmonica/CircleOfFifths.pm
@@ -1,16 +1,31 @@
-
-my @circle = qw /C G D A E B F# Db Ab Eb Bb F/;
-
-
-sub get_note {
-	my $self = shift;
-
-	my $start_note = shift;
-	my $offset = shift;
-
-	while ($offset > 12) {
-		
+#!/usr/bin/perl -w
+use strict;
 
 use Music::Scales;
 
+sub get_note {
+	#my $self = shift;
+	my $note = shift;
+	my $offset = shift;
 
+	if ($offset > 0) {
+		while ($offset-- > 0) {
+			my @maj = get_scale_notes ($note, 1);
+			$note = $maj[4];
+		}
+		return $note;
+
+	}
+	elsif ($offset < 0) {
+		while ($offset++ < 0) {
+			my @maj = get_scale_notes ($note, 1);
+			$note = $maj[3];
+		}
+		return $note;
+	} 
+	else {
+		return $note;
+	}
+}
+
+print get_note("C", 11);
