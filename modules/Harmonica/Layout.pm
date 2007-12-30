757e0a4bb338b2511a5fc58cb83f8b7ddbb319a2
jvaughan <jvaughan@31e0cd6d-f31f-0410-8c93-df718c08c370>
2007-12-30 14:14:01 +0000

    compiles but not functioning
    git-svn-id: svn+ssh://ln.turnip.org.uk/var/svn/jvaughan/trunk/dev/harplayout@115 31e0cd6d-f31f-0410-8c93-df718c08c370

--- modules/Harmonica/Layout.pm
+++ modules/Harmonica/Layout.pm
@@ -1,32 +1,45 @@
-Package Harmonica::Layout;
+package Harmonica::Layout;
 use strict;
 
+use Data::Dumper;
+
 use Class::MethodMaker
-	new_hash_with_init	=> 'new';
-	get_set			=> qw/tuning position key/
+	new_hash_with_init	=> 'new',
+	get_set			=> [ qw/tuning position key/ ]
 ;
+
+
+my $HOLES_INTERVALS = {
+        richter => {
+                blow => [qw /1 3 5 1 3 5 1 3 5 1/],
+                draw => [qw /2 5 7 2 4 6 7 2 4 6/],
+        }
+};
 
 
 sub init {
 	my $self = shift;
 
-	$self->{blow} = {};
-	$self->{draw} = {};
+	$self->{blow} = [];
+	$self->{draw} = [];
 
 	# Poupulate with data for natural notes
 	PLATE: foreach my $plate (qw /blow draw/) {
-		my @reeds = @$holes_intervals->{$self->tuning}->{$plate};
+		my @reeds = @{ $HOLES_INTERVALS->{richter}->{blow} };
 
-		REED: for (my $i = 0; $i < $#reeds $i++) {
+		REED: for (my $i = 0; $i < $#reeds + 1; $i++) {
 			$self->set_hole ($plate, $i + 1, 0, 
-				{note => $self>noteFromInterval ($reeds[$i]),
-				 position_interval => $self->positionInterval ($reeds[$i]),
-				 type => 'natural',
-				 tuning_offset => '+0',
+				{
+				position_interval => $self->positionInterval ($reeds[$i]),
+				type => 'natural',
+				tuning_offset => '+0',
+				note => $self->noteFromInterval($reeds[$i]),
 				}
 			);
 		}
 	}
+
+	print Dumper ($self);
 
 }
 
@@ -36,6 +49,8 @@ sub set_hole {
 
 	my ($plate, $hole, $bendstep, $attrs) = @_;
 
+	print Dumper ($attrs->{fuck});
+
 	foreach (keys %$attrs) {
 		$self->{ $plate }->[ $hole - 1 ]->[ $bendstep ]->{ $_ } = $attrs->{ $_ };
 	}
@@ -43,11 +58,17 @@ sub set_hole {
 
 
 
-sub noteFromInterval {
-	my $self = shift;
-
-}			 
-			
-				
 		
 
+sub positionInterval {
+	my $self = shift;
+	return '3';
+}
+
+sub noteFromInterval {
+	my $self = shift;
+	return 'Ab';
+}			 
+	
+
+1;
