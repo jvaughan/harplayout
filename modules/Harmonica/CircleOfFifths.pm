a929f00a29e23f10f93a209d57bf80425b5b49cc
jvaughan <jvaughan@31e0cd6d-f31f-0410-8c93-df718c08c370>
2007-12-30 18:27:50 +0000

    add intervalFromPosition sub
    git-svn-id: svn+ssh://ln.turnip.org.uk/var/svn/jvaughan/trunk/dev/harplayout@126 31e0cd6d-f31f-0410-8c93-df718c08c370

--- modules/Harmonica/CircleOfFifths.pm
+++ modules/Harmonica/CircleOfFifths.pm
@@ -1,4 +1,5 @@
 package Harmonica::CircleOfFifths;
+use strict;
 
 use Music::Scales;
 
@@ -29,7 +30,41 @@ sub noteFromPosition {
 	}
 }
 
-print get_note("C", 12);
+sub intervalFromPosition {
+	my $interval = shift;;
+	my $position = shift;
+
+	my $offset = $position -1;
+
+	my %clockwise = ( qw/
+		1	4
+		b2	b5
+		2	5
+		b3	b6
+		3	6
+		4	b7
+		b5	7
+		5	1
+		b6	b2
+		6	2
+		b7	b3
+		7	3
+	/);
+
+	my %anti_clockwise = reverse %clockwise;
+
+	if ($offset > 0) {
+                while ($offset-- > 0) {
+			$interval = $clockwise{$interval};
+                }
+                return $interval;
+
+        }
+}
+
+print "hello\n";
+print intervalFromPosition(1, 3);
+		
 print "\n";
 
 
