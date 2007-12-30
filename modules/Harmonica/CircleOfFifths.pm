dcdf0dc313ac47a5147640b8ba68b6d21aa69acb
jvaughan <jvaughan@31e0cd6d-f31f-0410-8c93-df718c08c370>
2007-12-30 14:57:10 +0000

    start c of 5s module
    git-svn-id: svn+ssh://ln.turnip.org.uk/var/svn/jvaughan/trunk/dev/harplayout@120 31e0cd6d-f31f-0410-8c93-df718c08c370

---
+++ modules/Harmonica/CircleOfFifths.pm
@@ -0,0 +1,16 @@
+
+my @circle = qw /C G D A E B F# Db Ab Eb Bb F/;
+
+
+sub get_note {
+	my $self = shift;
+
+	my $start_note = shift;
+	my $offset = shift;
+
+	while ($offset > 12) {
+		
+
+use Music::Scales;
+
+
