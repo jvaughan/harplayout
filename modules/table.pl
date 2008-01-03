5b598bd11aebca499ef3b2ecc0ddd6b084ff8fa7
jvaughan <jvaughan@31e0cd6d-f31f-0410-8c93-df718c08c370>
2008-01-03 16:59:54 +0000

    add table test script
    git-svn-id: svn+ssh://ln.turnip.org.uk/var/svn/jvaughan/trunk/dev/harplayout@203 31e0cd6d-f31f-0410-8c93-df718c08c370

---
+++ modules/table.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl -w 
+use strict;
+
+use Harmonica::Layout::Table;
+use Data::Dumper;
+
+my $harp = Harmonica::Layout::Table->new (position => '2', tuning => 'richter', key => 'F');
+#warn Dumper ($harp);
+
+$harp->printTable;
+
