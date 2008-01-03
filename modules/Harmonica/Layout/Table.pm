5b7a276fe88baa0fd9d882d8e8bd0fe073709f7e
jvaughan <jvaughan@31e0cd6d-f31f-0410-8c93-df718c08c370>
2008-01-03 16:59:37 +0000

    able to print a text table for blow plate
    git-svn-id: svn+ssh://ln.turnip.org.uk/var/svn/jvaughan/trunk/dev/harplayout@202 31e0cd6d-f31f-0410-8c93-df718c08c370

--- modules/Harmonica/Layout/Table.pm
+++ modules/Harmonica/Layout/Table.pm
@@ -1,24 +1,60 @@
 package Harmonica::Layout::Table;
 
+use Data::Dumper;
+
 use base qw(Harmonica::Layout);
 
 use Class::MethodMaker [
-	new 	=> [ -hash => -init => 'new' ],
-	scalar	=> [ qw/ position_key / ],
-	scalar	=> [					
+	new 	=> [ -hash => -init => 'new' ],				
 ];
 
 
 sub init {
 	my $self = shift;
 	
+	$self->SUPER::init;
+	
+	$self->{table} = {};
 	$self->_makeTable;
 }
 
 sub _makeTable {
 	my $self = shift;
 	
+	PLATE: foreach my $plate ( $self->plates )  {	
+		my @table;
+		my $hole = 0;
+		
+		REED: foreach ( $self->reeds($plate) ) {
+			$hole++;
+			my $bs = 0;
+			while (my $n = $self->get_note( $plate, $hole, $bs) ) {
+				$table[$bs][$hole -1] = $n;
+				#print "n: " . Dumper($n);
+				$bs++;
+			}
+		} # REED loop
+		$self->{table}->{$plate} = \@table;	
+	} # PLATE loop	
+}
+
+
+sub printTable {
+	my $self = shift;
 	
+	my @blow = @{ $self->{table}->{blow} };
+	
+	warn Dumper ($blow[0][0]);
+	
+	for my $bend (0 .. $#blow) {
+		#print "bb: " . Dumper ($blow[$bend]);
+		for my $reed (0 .. $#{ $blow[$bend] }) {
+			my $note = $blow[$bend][$reed];
+			my $fpi = defined $note ? $note->first_pos_interval : '';
+			print "|	$fpi";
+		}
+		print "\n";
+	}
 }
 
 1;
