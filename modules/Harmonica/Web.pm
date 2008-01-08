44477212d2383ecdfa3249b3bec1b1f26b4fa4a1
jvaughan <jvaughan@31e0cd6d-f31f-0410-8c93-df718c08c370>
2008-01-08 21:12:16 +0000

    put everything under one <form>, with differently valued submit buttons for the calculate modes.
    git-svn-id: svn+ssh://ln.turnip.org.uk/var/svn/jvaughan/trunk/dev/harplayout@270 31e0cd6d-f31f-0410-8c93-df718c08c370

--- modules/Harmonica/Web.pm
+++ modules/Harmonica/Web.pm
@@ -1,6 +1,7 @@
 package Harmonica::Web;
 use strict;
 
+use Switch;
 use base qw/ CGI::Application /;
 use CGI::Application::Plugin::AnyTemplate;
 use CGI::Application::Plugin::AutoRunmode;
@@ -40,17 +41,36 @@ sub showHarp : StartRunmode {
 	my %harp_params;
 	
 	if ( $self->submitted ) {
-		foreach my $f (@form_fields) {
+		foreach my $f ( @form_fields ) {
 			next unless Harmonica::Layout::Table->can($f);
 			$harp_params{$f} = $q->param($f) || 0;
 		}
-	}
-	
+
+		switch ( lc ($q->param('submit')) ) {
+			case 'get position' {
+				$harp_params{calculate} = 'position';
+				$harp_params{$_} = $q->param("calc_position-$_") || undef foreach (qw /key position_key/ );
+			}
+			case 'get harp key' {
+				$harp_params{calculate} = 'key';
+				$harp_params{$_} = $q->param("calc_key-$_") || undef foreach (qw /position position_key/ );
+			}
+			case 'get song key' {
+				$harp_params{calculate} = 'position_key';
+				$harp_params{$_} = $q->param("calc_position_key-$_") || undef foreach (qw /key position/ );
+			}
+			else {
+				$harp_params{$_} = $q->param($_) foreach qw/key position/;
+				$harp_params{calculate} = 'position_key';
+			}
+		}
+	} # Submitted?
+
 	my $harp = Harmonica::Layout::Table->new( %harp_params );
 	my $b = [$harp->blowNotes];	
 	my %template_params = (
 		harp	=> $harp,
-		debug	=> Dumper($b),
+		debug	=> '',
 	);
 	
 	$template_params{'calculate'} = $q->param('calculate') || 'position_key';
@@ -64,9 +84,6 @@ sub submitted {
 	my $self = shift;
 	return $self->query->param('submit') ? 1 : 0;
 }
-
-
-
 
 
 1;
