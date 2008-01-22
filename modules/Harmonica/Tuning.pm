f7d54ee375a300e25eac9ff11bdd9c852b5e9c2c
jvaughan <jvaughan@31e0cd6d-f31f-0410-8c93-df718c08c370>
2008-01-22 19:38:36 +0000

    add seydel bix six folk and circular
    git-svn-id: svn+ssh://ln.turnip.org.uk/var/svn/jvaughan/trunk/dev/harplayout@363 31e0cd6d-f31f-0410-8c93-df718c08c370

--- modules/Harmonica/Tuning.pm
+++ modules/Harmonica/Tuning.pm
@@ -14,10 +14,22 @@ my %tunings = (
 		draw => [qw / 2  4  6  7  2  4  6  7  2  4 /],
 	},
 
-	'Seydel Big Six' => {
+	'Seydel Big Six (blues)' => {
 		# Hole        1  2  3  4  5  6
 		blow => [qw / 1  3  5  1  3  5 /],
                 draw => [qw / 2  5  7  2  4  6 /],
+	},
+	
+	'Seydel Big Six (folk)' => {
+		# Hole        1  2  3  4  5  6
+		blow => [qw / 1  3  5  1  3  5 /],
+                draw => [qw / 2  4  6  7  2  4 /],
+	},
+	
+	'Seydel Circular' => {
+		# Hole        1  2  3  4  5  6  7  8  9 10
+		blow => [qw / 1  3  5 b7  2  4  6  1  3  5 /],
+		draw => [qw / 2  4  6  1  3  5 b7  2  4  6  /],
 	},
 
 	'Paddy Richter' => {
