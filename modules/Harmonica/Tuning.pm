a60309e0c6959ca3fbc901ccea90ddf5450127a4
jvaughan <jvaughan@31e0cd6d-f31f-0410-8c93-df718c08c370>
2008-01-22 19:47:57 +0000

    add country tuning
    git-svn-id: svn+ssh://ln.turnip.org.uk/var/svn/jvaughan/trunk/dev/harplayout@365 31e0cd6d-f31f-0410-8c93-df718c08c370

--- modules/Harmonica/Tuning.pm
+++ modules/Harmonica/Tuning.pm
@@ -38,6 +38,12 @@ my %tunings = (
                 draw => [qw / 2  5  7  2  4  6  7  2  4  6 /],
 	},
 	
+	'Country' => {
+		# Hole        1  2  3  4  5  6  7  8  9 10
+		blow => [qw / 1  3  5  1  3  5  1  3  5  1 /],
+                draw => [qw / 2  5  7  2 b5  6  7  2  4  6 /],
+	},
+	
 	'L.O Melody Maker (labelled in 2nd pos)' => {
 		# Hole        1  2  3  4  5  6  7  8  9 10
 		blow => [qw / 1  3  6  1  3  5  1  3  5  1 /],
