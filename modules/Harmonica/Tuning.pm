1d3cb0522eb366c4bd38f0d342ea21a71e32febb
jvaughan <jvaughan@31e0cd6d-f31f-0410-8c93-df718c08c370>
2008-01-23 17:45:28 +0000

    add 12 hole solo tuning
    git-svn-id: svn+ssh://ln.turnip.org.uk/var/svn/jvaughan/trunk/dev/harplayout@369 31e0cd6d-f31f-0410-8c93-df718c08c370

--- modules/Harmonica/Tuning.pm
+++ modules/Harmonica/Tuning.pm
@@ -13,6 +13,12 @@ my %tunings = (
 		blow => [qw / 1  3  5  1  1  3  5  1  1  3 /],
 		draw => [qw / 2  4  6  7  2  4  6  7  2  4 /],
 	},
+	
+	'Solo (12 hole)' => {
+		# Hole        1  2  3  4  5  6  7  8  9 10 11 12
+		blow => [qw / 1  3  5  1  1  3  5  1  1  3  5  1/],
+		draw => [qw / 2  4  6  7  2  4  6  7  2  4  6  7/],
+	},
 
 	'Seydel Big Six (blues)' => {
 		# Hole        1  2  3  4  5  6
