362a27c45b8c521a5845f3510126c599320e657b
Jon Vaughan <jon-git@turnip.org.uk>
2008-04-06 20:02:37 +0100

    use external function for tooltips - reduce html source size

--- js/forms.js
+++ js/forms.js
@@ -3,3 +3,33 @@ function handleChange (subval) {
     document.forms[0].js_submit.value = subval;
     document.forms[0].submit(); 
 }
+
+function makeTT (id, header, html) {
+    document.observe('dom:loaded', function() {
+    
+    if(navigator.userAgent.search(/msie/i)!= -1) {
+        // Don't use scriptaculous effect on msie - not smooth
+        new Tip(
+	    id,
+	    html,
+	    {
+	        title:   header,
+	        className: 'harptt'
+	    }
+	);
+    } else {
+        // Non-ie Code
+        new Tip(
+    	    id, 
+    	    html,
+    	    {
+    	        title:   header,
+    	        effect:  'appear',
+    	        duration: '0.2',
+    	        className: 'harptt'
+    	    }
+    	);
+    }
+
+    });
+}
