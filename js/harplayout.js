jvaughan/harplayout

commit ea7ee9a12f57ca8a8ca7becde0dbca1441a89902

Toggle notes / intervals / interval_categories instead of form reload

js/harplayout.js
@@ -14,6 +14,14 @@ jQuery(document).ready(
 
 function handleChange (subval) {
 	document.forms[0].js_submit.value = subval;
+	
+	if (subval == 'note'
+	 || subval == 'interval'
+	 || subval == 'interval_category') {
+		var class = '.' + subval;
+		jQuery(class).toggle();
+		return;
+	}
 	reloadFormAndHarp();
 }
 

templates/inc/harptable/notecell.tmpl
@@ -12,9 +12,9 @@ makeTT('[% note.id %]', '[% note.description %]',  '[% PROCESS "inc/harptable/to
 //]]>
 </script>
     <table class="notecell">
-        [% IF harp.show_intervals %]<tr><td class="interval">[% note.position_interval %]</td></tr>[% END %]
-        [% IF harp.show_notes %]<tr><td class="note">[% note.note %]</td></tr>[% END %]
-        [% IF harp.show_interval_categories %]<tr><td class="interval_category"><span class="[% note.interval_category %]">&bull;</span></td></tr>[% END %]
+        <tr><td class="interval"  [% UNLESS harp.show_intervals %] style="display:none" [% END %]>[% note.position_interval %]</td></tr>
+        <tr><td class="note" [% UNLESS harp.show_notes %] style="display:none" [% END %]>[% note.note %]</td></tr>
+        <tr><td class="interval_category" [% UNLESS harp.show_interval_categories %] style="display:none" [% END %]><span class="[% note.interval_category %]">&bull;</span></td></tr>
     </table>
     <!-- >[% note.position_interval %] -->
 [% END %]

templates/inc/view_options.tmpl
@@ -32,21 +32,22 @@
     name        = 'show_notes'
     checked     = harp.show_notes
     label       = 'Note names'
-    onchange    = "handleChange('change options')"
+    onchange    = "handleChange('note')"
+    id          = 'show_notes_id'
 %]   
 <br />
 [% INCLUDE 'form/checkbox.tmpl'
     name        = 'show_intervals'
     checked     = harp.show_intervals
     label       = 'Intervals'
-    onchange    = "handleChange('change options')"
+    onchange    = "handleChange('interval')"
 %]
 <br />
 [% INCLUDE 'form/checkbox.tmpl'
     name        = 'show_interval_categories'
     checked     = harp.show_interval_categories
     label       = 'Interval categories'
-    onchange    = "handleChange('change options')"
+    onchange    = "handleChange('interval_category')"
 %]
 </div>
 </div>

commit b6dc99e4782db627019bc0ad4a8b24c9942d38c0

Fix JS bug which prevented Safari & msie from working correctly.
bug introduced in ea7ee9a12f57ca8a8ca7becde0dbca1441a89902

js/harplayout.js
@@ -18,8 +18,7 @@ function handleChange (subval) {
 	if (subval == 'note'
 	 || subval == 'interval'
 	 || subval == 'interval_category') {
-		var class = '.' + subval;
-		jQuery(class).toggle();
+		jQuery('.' + subval).toggle();
 		return;
 	}
 	reloadFormAndHarp();